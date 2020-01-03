88824741853d389a03bfe0bfcca7b92e828757fc
Grant Miller <grant242@gmail.com>
2020-01-02 23:08:38 -0500

    basic generator is working

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import requests
+import ast
 
-url = 'https://www.basketballforcoaches.com/basketball-terms/'
+with open('resources/terms.txt') as f:
+    terms = ast.literal_eval(f.read())
 
+with open('resources/nounlist.txt') as f:
+    nouns = f.read()
 
+nouns = set(nouns.split('\n'))
 
-if __name__ == '__main__':
-    pass
+nouns.remove('')
+
+for i in range(10):
+    term = terms.pop()
+    noun = nouns.pop()
+    print(f'The {term} is like a {noun} if you think about it.')
+
+# if __name__ == '__main__':
+#     pass
