a6c8c9bb4713d9fb47fb0dfa009e10d5efe6a115
Grant Miller <grant242@gmail.com>
2020-01-03 22:07:43 -0500

    ngram update

--- ngram.py
+++ ngram.py
@@ -2,14 +2,13 @@ from string import punctuation
 import random
 
 with open('resources/speeches.txt') as f:
-    text = f.read().replace('\n',' ')
+    text = f.read().replace('\n',' ').lower()
 
-text = text.translate(str.maketrans('','',punctuation)).replace('','',)
+# text = text.translate(str.maketrans('','',punctuation)).replace('','',)
 words = text.split()
-# print(words[100:400])
 
-def generate_markov(words,gram_size=2):
-    markov = {}
+def generate_markov(words,gram_size=2,markov=None):
+    markov = {} if markov is None else markov
     for i,word in enumerate(words[:len(words)-gram_size+1]):
         # if i + gram_size < len(words):
         phrase = tuple(words[j] for j in range(i,i+gram_size-1))
@@ -20,6 +19,18 @@ def generate_markov(words,gram_size=2):
 
     return markov
 
+def generate_markov_sentences(text, gram_size=2):
+    markov = {}
+    sentences = text.split('.')
+    for sentence in sentences:
+        words = sentence.split()
+        # print(sentence)
+        # words.append('.')
+        markov = generate_markov(words,gram_size,markov)
+    
+    return markov
+
+
 def generate_phrase(markov, phrase_len, gram_size):
     first_section = random.choice(list(markov))
     # print(' '.join(first_section))
@@ -28,10 +39,11 @@ def generate_phrase(markov, phrase_len, gram_size):
         prefix = tuple(phrase[j] for j in range(i-gram_size,i-1))
         if prefix in markov:
             next_word = random.choice(markov[prefix])
+            phrase.append(next_word)
         else:
             phrase.append('.')
             phrase.extend(generate_phrase(markov, phrase_len-len(phrase), gram_size))
-        phrase.append(next_word)
+        
 
     return phrase
     # print(phrase)
@@ -39,10 +51,12 @@ def generate_phrase(markov, phrase_len, gram_size):
 
 
 
-markov = generate_markov(words, 3)
+markov = generate_markov_sentences(text, 3)
 phrase = generate_phrase(markov, 10, 3)
+# print(markov[('going','plays')])
+# print(markov)
 
-print((' '.join(phrase)))
+print((' '.join(phrase).replace(' . ','. ')))
 # print(markov[('honeymoons',)])
 
 
